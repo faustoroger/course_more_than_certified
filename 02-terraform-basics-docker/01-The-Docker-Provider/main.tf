27d73849aa09a7719f087836aa9f9a81d7074db9
Fausto Roger <hixian@gmail.com>
2023-01-19 13:20:06 -0300

    add splat expression

--- 02-terraform-basics-docker/01-The-Docker-Provider/main.tf
+++ 02-terraform-basics-docker/01-The-Docker-Provider/main.tf
@@ -32,21 +32,21 @@ resource "docker_container" "nodered_container" {
 
 
 output "ip-address" {
-  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+  value       = join(":", flatten([docker_container.nodered_container[*].ip_address, docker_container.nodered_container[*].ports[0].external]))
   description = "The IP address and external port of the container"
 }
 
 output "container-name" {
-  value       = docker_container.nodered_container[0].name
+  value       = docker_container.nodered_container[*].name
   description = "The name of the container"
 }
 
-output "ip-address2" {
-  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
-  description = "The IP address and external port of the container"
-}
+# output "ip-address2" {
+#   value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+#   description = "The IP address and external port of the container"
+# }
 
-output "container-name2" {
-  value       = docker_container.nodered_container[1].name
-  description = "The name of the container"
-}
+# output "container-name2" {
+#   value       = docker_container.nodered_container[1].name
+#   description = "The name of the container"
+# }
