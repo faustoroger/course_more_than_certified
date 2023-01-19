ef4b61c6caf89aca618358c362c8ec1a460ced4d
Fausto Roger <hixian@gmail.com>
2023-01-19 13:07:38 -0300

    add the join function and random_string resource

--- 02-terraform-basics-docker/01-The-Docker-Provider/main.tf
+++ 02-terraform-basics-docker/01-The-Docker-Provider/main.tf
@@ -1,34 +1,52 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.15.0"
+      source = "kreuzwerker/docker"
     }
   }
 }
 
-provider "docker" {
-}
+provider "docker" {}
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  count   = 2
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
   }
 }
 
-output "ip_address" {
-  value = docker_container.nodered_container.ip_address  
-  description = "The IP address of the container"
+
+output "ip-address" {
+  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+  description = "The IP address and external port of the container"
 }
 
 output "container-name" {
-  value = docker_container.nodered_container.name
+  value       = docker_container.nodered_container[0].name
+  description = "The name of the container"
+}
+
+output "ip-address2" {
+  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+  description = "The IP address and external port of the container"
+}
+
+output "container-name2" {
+  value       = docker_container.nodered_container[1].name
   description = "The name of the container"
 }
