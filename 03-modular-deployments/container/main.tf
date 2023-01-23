f27177d5a50a295c4aea121059e75210f7351c19
Fausto Roger <hixian@gmail.com>
2023-01-23 19:35:03 -0300

    add dynamic block

--- 03-modular-deployments/container/main.tf
+++ 03-modular-deployments/container/main.tf
@@ -13,22 +13,25 @@ resource "docker_container" "app_container" {
     internal = var.int_port_in
     external = var.ext_port_in[count.index]
   }
-  volumes {
-    container_path = var.container_path_in
-    volume_name    = docker_volume.container_volume[count.index].name
+  dynamic "volumes" {
+    for_each = var.volumes_in
+    content {
+      container_path = volumes.value["container_path_each"]
+      volume_name    = docker_volume.container_volume[volumes.key].name
+    }
   }
   provisioner "local-exec" {
-    command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> containers.txt"
+    command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> test.txt"
   }
   provisioner "local-exec" {
     when    = destroy
-    command = "rm -f containers.txt"
+    command = "rm -f test.txt"
   }
 }
 
 resource "docker_volume" "container_volume" {
-  count = var.count_in
-  name  = "${var.name_in}-${random_string.random[count.index].result}-volume"
+  count = length(var.volumes_in)
+  name  = "${var.name_in}-volume-${count.index}"
   lifecycle {
     prevent_destroy = false
   }
@@ -39,8 +42,7 @@ resource "docker_volume" "container_volume" {
   }
   provisioner "local-exec" {
     when       = destroy
-    # command    = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
-    command    = "tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
+    command    = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
     on_failure = fail
   }
 }
