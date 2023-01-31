cd402950fb1a2181489e888f05f2ae284fd149de
Fausto Roger <hixian@gmail.com>
2023-01-31 17:52:52 -0300

    add ec2 resource and edit gitignore file

--- 04-AWS-K8s/main.tf
+++ 04-AWS-K8s/main.tf
@@ -1,30 +1,30 @@
 # --- root/main.tf ---
+
 module "networking" {
   source           = "./networking"
   vpc_cidr         = local.vpc_cidr
-  access_ip        = var.access_ip
-  security_groups  = local.security_groups
   private_sn_count = 3
   public_sn_count  = 2
-  private_cidrs    = [for i in range(1, 255, 2) : cidrsubnet("10.123.0.0/16", 8, i)]
-  public_cidrs     = [for i in range(2, 255, 2) : cidrsubnet("10.123.0.0/16", 8, i)]
+  private_cidrs    = [for i in range(1, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
+  public_cidrs     = [for i in range(2, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
   max_subnets      = 20
-  db_subnet_group  = true
+  access_ip        = var.access_ip
+  security_groups  = local.security_groups
+  db_subnet_group  = "true"
 }
 
-# module "database" {
-#   source                 = "./database"
-#   db_storage             = 10
-#   db_engine_version      = "5.7.22"
-#   db_instance_class      = "db.t2.micro"
-#   dbname                 = "rancher"
-#   dbuser                 = "bobby"
-#   dbpassword             = "t4b!3s2021"
-#   db_identifier          = "mtc-db"
-#   skip_db_snapshot       = true
-#   db_subnet_group_name   = module.networking.db_subnet_group_name[0]
-#   vpc_security_group_ids = module.networking.db_security_group
-# }
+module "database" {
+  source                 = "./database"
+  db_engine_version      = "5.7.22"
+  db_instance_class      = "db.t2.micro"
+  dbname                 = var.dbname
+  dbuser                 = var.dbuser
+  dbpassword             = var.dbpassword
+  db_identifier          = "mtc-db"
+  skip_db_snapshot       = true
+  db_subnet_group_name   = module.networking.db_subnet_group_name[0]
+  vpc_security_group_ids = [module.networking.db_security_group]
+}
 
 module "loadbalancing" {
   source                  = "./loadbalancing"
@@ -37,6 +37,23 @@ module "loadbalancing" {
   elb_unhealthy_threshold = 2
   elb_timeout             = 3
   elb_interval            = 30
-  listener_port           = 8000
+  listener_port           = 80
   listener_protocol       = "HTTP"
+}
+
+module "compute" {
+  source              = "./compute"
+  public_sg           = module.networking.public_sg
+  public_subnets      = module.networking.public_subnets
+  instance_count      = 2
+  instance_type       = "t3.micro"
+  vol_size            = "20"
+  public_key_path     = "/home/ubuntu/.ssh/mtckey.pub"
+  key_name            = "mtckey"
+  dbname              = var.dbname
+  dbuser              = var.dbuser
+  dbpassword          = var.dbpassword
+  db_endpoint         = module.database.db_endpoint
+  user_data_path      = "${path.root}/userdata.tpl"
+  lb_target_group_arn = module.loadbalancing.lb_target_group_arn
 }
--- 04-AWS-K8s/variables.tf
+++ 04-AWS-K8s/variables.tf
@@ -1,7 +1,5 @@
 # --- root/variables.tf ---
 variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
-  type        = string
   default     = "us-east-1"
 }
 
