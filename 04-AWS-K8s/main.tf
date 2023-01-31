09222306cef09b117dacb393f408f431da8dd88d
Fausto Roger <hixian@gmail.com>
2023-01-31 16:29:12 -0300

    add alb

--- 04-AWS-K8s/main.tf
+++ 04-AWS-K8s/main.tf
@@ -12,16 +12,31 @@ module "networking" {
   db_subnet_group  = true
 }
 
-module "database" {
-  source                 = "./database"
-  db_storage             = 10
-  db_engine_version      = "5.7.22"
-  db_instance_class      = "db.t2.micro"
-  dbname                 = "rancher"
-  dbuser                 = "bobby"
-  dbpassword             = "t4b!3s2021"
-  db_identifier          = "mtc-db"
-  skip_db_snapshot       = true
-  db_subnet_group_name   = module.networking.db_subnet_group_name[0]
-  vpc_security_group_ids = module.networking.db_security_group
+# module "database" {
+#   source                 = "./database"
+#   db_storage             = 10
+#   db_engine_version      = "5.7.22"
+#   db_instance_class      = "db.t2.micro"
+#   dbname                 = "rancher"
+#   dbuser                 = "bobby"
+#   dbpassword             = "t4b!3s2021"
+#   db_identifier          = "mtc-db"
+#   skip_db_snapshot       = true
+#   db_subnet_group_name   = module.networking.db_subnet_group_name[0]
+#   vpc_security_group_ids = module.networking.db_security_group
+# }
+
+module "loadbalancing" {
+  source                  = "./loadbalancing"
+  public_sg               = module.networking.public_sg
+  public_subnets          = module.networking.public_subnets
+  tg_port                 = 8000
+  tg_protocol             = "HTTP"
+  vpc_id                  = module.networking.vpc_id
+  elb_healthy_threshold   = 2
+  elb_unhealthy_threshold = 2
+  elb_timeout             = 3
+  elb_interval            = 30
+  listener_port           = 8000
+  listener_protocol       = "HTTP"
 }
--- 04-AWS-K8s/networking/outputs.tf
+++ 04-AWS-K8s/networking/outputs.tf
@@ -1,4 +1,5 @@
 # --- networking/outputs.tf ---
+
 output "vpc_id" {
   value = aws_vpc.mtc_vpc.id
 }
@@ -8,5 +9,13 @@ output "db_subnet_group_name" {
 }
 
 output "db_security_group" {
-  value = [aws_security_group.mtc_sg["rds"].id]
+  value = aws_security_group.mtc_sg["rds"].id
+}
+
+output "public_sg" {
+  value = aws_security_group.mtc_sg["public"].id
+}
+
+output "public_subnets" {
+  value = aws_subnet.mtc_public_subnet.*.id
 }
