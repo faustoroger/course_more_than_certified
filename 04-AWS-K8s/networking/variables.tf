abc5de4a3f64210ab796923dc49af6e8ca50cad2
Fausto Roger <hixian@gmail.com>
2023-01-31 16:29:12 -0300

    add alb

--- 04-AWS-K8s/networking/variables.tf
+++ 04-AWS-K8s/networking/variables.tf
@@ -1,35 +1,11 @@
 # --- networking/variables.tf ---
-variable "vpc_cidr" {
-  type = string
-}
 
-variable "public_cidrs" {
-  type = list(any)
-}
-
-variable "private_cidrs" {
-  type = list(any)
-}
-
-variable "public_sn_count" {
-  type = number
-}
-
-variable "private_sn_count" {
-  type = number
-}
-
-variable "max_subnets" {
-  type = number
-}
-
-variable "access_ip" {
-  type = string
-}
-
-variable "security_groups" {
-}
-
-variable "db_subnet_group" {
-  type = bool
-}
+variable "vpc_cidr" {}
+variable "public_cidrs" {}
+variable "private_cidrs" {}
+variable "public_sn_count" {}
+variable "private_sn_count" {}
+variable "max_subnets" {}
+variable "access_ip" {}
+variable "security_groups" {}
+variable "db_subnet_group" {}
