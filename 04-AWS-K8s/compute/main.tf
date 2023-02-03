8c978e615fe870547605c6f4146ba0710f432130
Fausto Roger <hixian@gmail.com>
2023-02-02 22:23:20 -0300

    add code to use k3s

--- 04-AWS-K8s/compute/main.tf
+++ 04-AWS-K8s/compute/main.tf
@@ -1,5 +1,3 @@
-# --- compute/main.tf ---
-
 data "aws_ami" "server_ami" {
   most_recent = true
 
@@ -50,11 +48,30 @@ resource "aws_instance" "mtc_node" {
   root_block_device {
     volume_size = var.vol_size
   }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = self.public_ip
+      private_key = file("/home/ubuntu/.ssh/mtckey")
+    }
+    script = "${path.root}/delay.sh"
+  }
+  provisioner "local-exec" {
+    command = templatefile("${path.cwd}/scp_script.tpl",
+      {
+        nodeip   = self.public_ip
+        k3s_path = "${path.cwd}/../"
+        nodename = self.tags.Name
+      }
+    )
+  }
 }
 
 resource "aws_lb_target_group_attachment" "mtc_tg_attach" {
   count            = var.instance_count
   target_group_arn = var.lb_target_group_arn
   target_id        = aws_instance.mtc_node[count.index].id
-  port             = var.tg_port
+  port             = 8000
 }
